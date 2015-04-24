cffab4b40dfb1a812d5861943f919a21979611f9
Chris Ballance <cballance@boardbooks.com>
2015-04-24 19:07:35 -0400

    Moved some things around.

---
+++ Ballance.StarDate.Tests/StarDateTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ballance.StarDate.Tests
+{
+    [TestFixture]
+    public class StarDateTests
+    {
+        [Test]
+        public void ShouldReturnCorrectStarDate_WhenKnownEarthDatePassedIn()
+        {
+            var tngKnownDate = new DateTime(2318, 7, 5, 12, 0, 0);
+            var equvalentStarDate = tngKnownDate.ToStarDate();
+            Assert.AreEqual(0d, equvalentStarDate);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectStarDate_WhenSendingToday_April_24_2015()
+        {
+            var tngKnownDate = new DateTime(2015, 4, 24, 12, 0, 0);
+            var equvalentStarDate = tngKnownDate.ToStarDate();
+            Assert.AreEqual(-278404.30d, equvalentStarDate);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectHomesteadDate_WhenSending_April_6_2378()
+        {
+            var homesteadDate = new DateTime(2378, 4, 6, 12, 0, 0);
+            var equivalentStarDate = homesteadDate.ToStarDate();
+            Assert.AreEqual(54868.82d, equivalentStarDate);
+        }
+    }
+}
---
+++ Ballance.StarDate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ballance.StarDate")]
+[assembly: AssemblyDescription("Earth date to StarDate converter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ballance.it")]
+[assembly: AssemblyProduct("Ballance.StarDate")]
+[assembly: AssemblyCopyright("Copyright © 2015 Chris Ballance")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7253ed66-55f2-4fa4-8828-1b957d797f4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
