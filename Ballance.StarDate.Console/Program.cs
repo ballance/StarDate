5d81ff5111b137845f40fca959e500d248592cfe
Chris Ballance <cballance@boardbooks.com>
2015-04-24 19:17:23 -0400

    Added Console app

---
+++ Ballance.StarDate.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ballance.StarDate.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a date");
+            var stringDate = System.Console.ReadLine();
+
+            var parsedDate = DateTime.Parse(stringDate);
+
+            var convertedToStarDate = parsedDate.ToStarDate();
+
+            System.Console.WriteLine("Equivalent TNG Star Date: {0}", convertedToStarDate);
+            System.Console.ReadKey();
+        }
+    }
+}
